378bf0c121d52175f8ee1f33273cc60c0d666a48
Menamonmon <monyasser111@gmail.com>
2020-08-09 12:02:45 -0400

    Recreated Navbar and created NavItems + some style changes

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,22 +1,27 @@
 import React from 'react';
 import './navbar.css';
 
-export default class NavBar extends React.Component {
-
-  render() {
-    let navLinks = this.props.navLinks;
-    navLinks = navLinks.map(l => <li><a href={l.link}>{l.pageName}</a></li>);
-    console.log(navLinks.length);
-
-    return (
-      <header className="nav-header">
-        <img src={"./images/img/dhs-logo.png"} className="logo"/>
-        <nav className="navbar">
-          <ul className="nav-ul">
-            {navLinks}
-          </ul>
-        </nav>
-      </header>
-    );
-  }
+export function Navbar(props) {
+  return (
+    <nav className="navbar">
+      {props.navLogo}
+      <ul className="nav-ul">{props.children}</ul>
+    </nav>
+  );
 }
+
+export function NavIcon(props) {
+  return (
+    <img src={props.src} alt="logo" className="nav-icon"/>
+  );
+}
+
+export function NavItem(props) {
+  return (
+    <li name="nav-item" title={props.title} className="nav-item" key={Math.random().toString()}>
+      <a title={props.title} href={props.link} className="icon-btn">
+        {props.icon}
+      </a>
+    </li>
+  )
+} 
